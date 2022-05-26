a4a40381f000bb677c3e65cf8d0d8b9e9747b27f
mgorski1 <m.gorski.1@stud.elka.pw.edu.pl>
2022-05-27 00:21:11 +0200

    saving uploaded image in folder

--- rest/app.py
+++ rest/app.py
@@ -1,7 +1,21 @@
+import os
+import logging
+import base64
+
 # using flask_restful
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, flash
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+
+# Variables to be defined and changed soon
+UPLOAD_FOLDER = r'D:\Dokumenty\CBIR\repo_scripts\CBIRSolver\test_upload'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('HELLO WORLD')
 
 # creating the flask app
 app = Flask(__name__)
@@ -38,10 +52,38 @@ class Square(Resource):
 
         return jsonify({'square': num**2})
 
+# my rest functions
+
+
+class Image(Resource):
+
+    def post(self):
+        # data = request.get_json()
+        # file_name = data['file_name']
+        target = UPLOAD_FOLDER
+        if not os.path.isdir(target):
+            os.mkdir(target)
+        logger.info("welcome to upload")
+        if 'file' not in request.files:
+            logger.info('No file part')
+        file = request.files['file']
+        name = file.filename
+        if name == '':
+            logger.info('No selected file')
+            name = 'sub.jpg'
+        destination = "\\".join([target, name])
+        logger.info("trying to save file ")
+        file.save(destination)
+        response = "File sucessfully saved on server"
+        return response
+        # obj = jsonify({'message': 'image saved as: ' + file_name})
+        # return obj
+
 
 # adding the defined resources along with their corresponding urls
 api.add_resource(Hello, '/')
 api.add_resource(Square, '/square/<int:num>')
+api.add_resource(Image, '/uploadImage/')
 
 
 # driver function
